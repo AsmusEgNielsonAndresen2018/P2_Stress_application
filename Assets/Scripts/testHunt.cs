4aa7da1796747c8a73a7d82872ccfae1c65f1e4b
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2019-04-22 13:48:36 +0200

    hunt progression
    working on display hunts

---
+++ Assets/Scripts/testHunt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testHunt : MonoBehaviour
+{
+    public GameObject canvasHolder;
+    public GameObject placeHolder;
+    public GameObject nameholders;
+    string canvasName = "Canvas";
+    string[] huntNames = {"yes i did sir","have you seen my bear, tibbers" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        hunt[] hunts = new hunt[2];
+
+        hunts[0].setHuntName(huntNames[0]);
+        Debug.Log(hunts[0].getHuntName());
+            /*canvasHolder = GameObject.Find(canvasName);
+            GameObject newPlaceHolder = Instantiate(placeHolder, canvasHolder.transform);
+            nameholders = placeHolder.transform.GetChild(0).gameObject;
+            hunts[0].getHuntName(nameholders);  */      
+    }
+
+   
+}
